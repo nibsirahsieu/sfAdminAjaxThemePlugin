6ce785e27c2781d3d41e93fb3ac37d58be98b217
nibsirahsieu <nibsirahsieu@gmail.com>
2010-11-26 09:55:52 +0700

    [admin.form.js] Adding a note + fixing some formatting issues

--- web/js/admin.form.js
+++ web/js/admin.form.js
@@ -1,26 +1,18 @@
+//based on http://www.tutorialswitch.com/web-development/quick-and-simple-ajax-forms-with-json-responses/
+
 function setupAjaxForm(form_id, form_validations){
 	var form = '#' + form_id;
 	
-	// en/disable submit button
-	var disableSubmit = function(val){
-		$(form + ' input[type=submit]').attr('disabled', val);
-	};
-	
-	// setup jQuery Plugin 'ajaxForm' 	
-	var options = {
-		dataType:  'json',
+  var options = {
+    dataType:  'json',
     url: $(form).attr('action')+'?&ajaxify=1',
-		beforeSubmit: function(){
-      // run form validations if they exist
-			if(typeof form_validations == "function" && !form_validations()) {
-				// this will prevent the form from being subitted
-				return false;
-			}
-		},
-    //beforeSend: function() { disableSubmit(true); },
-		success: function(json){
-      //disableSubmit(false);
-			if (json.type == 'error') {
+    beforeSubmit: function(){
+      if(typeof form_validations == "function" && !form_validations()) {
+        return false;
+      }
+    },
+    success: function(json){
+      if (json.type == 'error') {
         var errs = json.errs;
         $(form).find('span.error').remove();
         $.each(errs, function (index, value){
@@ -33,6 +25,6 @@ function setupAjaxForm(form_id, form_validations){
       }
     },
     error: function (XMLHttpRequest, textStatus, errorThrown) {}
-	};
-	$(form).ajaxForm(options);
+  };
+  $(form).ajaxForm(options);
 }
