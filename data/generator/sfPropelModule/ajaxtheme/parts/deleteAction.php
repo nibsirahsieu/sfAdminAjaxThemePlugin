93c154382fcbeba66ca24c3e11de6e7fdfcb96f0
nibsirahsieu <nibsirahsieu@gmail.com>
2010-11-26 11:34:43 +0700

    [ajaxtheme] Adding support for internationalization

--- data/generator/sfPropelModule/ajaxtheme/parts/deleteAction.php
+++ data/generator/sfPropelModule/ajaxtheme/parts/deleteAction.php
@@ -9,15 +9,15 @@
     $this->getUser()->setFlash('notice', 'The item was deleted successfully.');
 
     if ($request->isXmlHttpRequest()) {
+      sfProjectConfiguration::getActive()->loadHelpers(array('I18N'));
       sfConfig::set('sf_web_debug', false);
       $this->getResponse()->setContentType('application/json');
 
       $response['type'] = 'notice';
-      $response['msg'] = 'The item was deleted successfully.';
+      $response['msg'] = __('The item was deleted successfully.', array(), 'sf_admin');
 
       $response['redirectToUrl'] = $this->getController()->genUrl('@<?php echo $this->getUrlForAction('list') ?>');
       return $this->renderText(json_encode($response));
-
     } else {
       $this->redirect('@<?php echo $this->getUrlForAction('list') ?>');
     }
