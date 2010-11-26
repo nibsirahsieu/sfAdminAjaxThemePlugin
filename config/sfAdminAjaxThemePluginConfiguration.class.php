6802d409b638d7b48ad05e2771c335a180253867
nibsirahsieu <nibsirahsieu@gmail.com>
2010-11-26 23:39:22 +0700

    [sfAdminAjaxThemePluginConfiguration] fix typo

--- config/sfAdminAjaxThemePluginConfiguration.class.php
+++ config/sfAdminAjaxThemePluginConfiguration.class.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
- * sfAjaxThemePluginConfiguration configuration.
+ * sfAdminAjaxThemePluginConfiguration configuration.
  *
- * @package     sfAjaxThemePluginConfiguration
+ * @package     sfAdminAjaxThemePluginConfiguration
  * @subpackage  config
  * @author      Nibsirahsieu <nibsirahsieu@gmail.com>
  * @version     SVN: $Id: PluginConfiguration.class.php 12675 2010-11-21 08:07:42Z Kris.Wallsmith $
